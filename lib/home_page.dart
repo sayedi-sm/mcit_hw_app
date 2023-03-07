754028dfff3a7c59291f53469ee2a54f4b8bfa66
Sayed Mahmood Sayedi <mahmood.243f3@gmail.com>
2023-03-07 11:52:20 +0430

    Javascript class work

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _findController = TextEditingController(),
+      _replaceController = TextEditingController(),
+      _resultController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +18,50 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: const Center(
-        child: Text('Hi there!'),
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _findController,
+                decoration: const InputDecoration(
+                  hintText: 'Find',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _replaceController,
+                decoration: const InputDecoration(
+                  hintText: 'Replace',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _resultController.text = _resultController.text.replaceAll(
+                        _findController.text.trim(),
+                        _replaceController.text.trim());
+                  });
+                },
+                child: const Text('Replace'),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _resultController,
+                maxLines: 8,
+                decoration: const InputDecoration(
+                  hintText: 'Your text here!',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
